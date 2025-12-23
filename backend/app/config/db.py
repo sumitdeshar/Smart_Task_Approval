1c5185f558c79684a67acb1bb5b2a1230e60be69
Sumit Deshar <sumeetdsr@gmail.com>
2025-12-23 23:47:37 +0545

    docker container restarting error

---
+++ backend/app/config/db.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
+
+print("DATABASE_URL =", DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from config.db import engine
+from app import models
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
